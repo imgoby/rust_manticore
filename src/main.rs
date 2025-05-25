9d4cd98566805c2aca3b337cd2cb5399fd5943a3
goby <imgoby@163.com>
2025-05-25 19:09:21 +0800

    采用query_drop才支持manticore

--- src/main.rs
+++ src/main.rs
@@ -18,31 +18,33 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let mut conn = pool.get_conn()?;
 
     // Let's create a table for payments.
-    // conn.query_drop(
-    //     r"CREATE TEMPORARY TABLE payment (
-    //         customer_id int not null,
-    //         amount int not null,
-    //         account_name text
-    //     )")?;
+    conn.query_drop(
+        r"CREATE TABLE IF NOT EXISTS payment (
+            customer_id int ,
+            amount int ,
+            account_name string
+        )")?;
 
-    let payments = vec![
-        Payment { customer_id: 1, amount: 2, account_name: None },
-        Payment { customer_id: 3, amount: 4, account_name: Some("foo".into()) },
-        Payment { customer_id: 5, amount: 6, account_name: None },
-        Payment { customer_id: 7, amount: 8, account_name: None },
-        Payment { customer_id: 9, amount: 10, account_name: Some("bar".into()) },
-    ];
+    // let payments = vec![
+    //     Payment { customer_id: 1, amount: 2, account_name: None },
+    //     Payment { customer_id: 3, amount: 4, account_name: Some("foo".into()) },
+    //     Payment { customer_id: 5, amount: 6, account_name: None },
+    //     Payment { customer_id: 7, amount: 8, account_name: None },
+    //     Payment { customer_id: 9, amount: 10, account_name: Some("bar".into()) },
+    // ];
+
+    conn.query_drop("INSERT INTO payment (customer_id, amount, account_name) VALUES (1,2,'tom')")?;
 
     // Now let's insert payments to the database
-    conn.exec_batch(
-        r"INSERT INTO payment (customer_id, amount, account_name)
-          VALUES (:customer_id, :amount, :account_name)",
-        payments.iter().map(|p| params! {
-            "customer_id" => p.customer_id,
-            "amount" => p.amount,
-            "account_name" => &p.account_name,
-        })
-    )?;
+    // conn.exec_batch(
+    //     r"INSERT INTO payment (customer_id, amount, account_name)
+    //       VALUES (:customer_id, :amount, :account_name)",
+    //     payments.iter().map(|p| params! {
+    //         "customer_id" => p.customer_id,
+    //         "amount" => p.amount,
+    //         "account_name" => &p.account_name,
+    //     })
+    // )?;
 
     // Let's select payments from database. Type inference should do the trick here.
     let selected_payments = conn
@@ -53,11 +55,17 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
             },
         )?;
 
+    for item in selected_payments{
+        print!("{:?}",item);
+    }
+
+
+
     // Let's make sure, that `payments` equals to `selected_payments`.
     // Mysql gives no guaranties on order of returned rows
     // without `ORDER BY`, so assume we are lucky.
-    assert_eq!(payments, selected_payments);
-    println!("Yay!");
+    // assert_eq!(payments, selected_payments);
+    // println!("Yay!");
 
     Ok(())
 }
